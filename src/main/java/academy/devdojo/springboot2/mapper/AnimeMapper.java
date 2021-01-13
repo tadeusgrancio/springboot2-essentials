da51428514a929201158829a8c5edfbec1794102
Anderson Tadeu Sgrancio <31711896+tadeusgrancio@users.noreply.github.com>
2021-01-13 12:02:33 -0300

    Update AnimeMapper.java
    Correção no nome da variável

--- src/main/java/academy/devdojo/springboot2/mapper/AnimeMapper.java
+++ src/main/java/academy/devdojo/springboot2/mapper/AnimeMapper.java
@@ -12,5 +12,5 @@ public abstract class AnimeMapper {
 
     public abstract Anime toAnime(AnimePostRequestBody animePostRequestBody);
 
-    public abstract Anime toAnime(AnimePutRequestBody animePostRequestBody);
+    public abstract Anime toAnime(AnimePutRequestBody animePutRequestBody);
 }
